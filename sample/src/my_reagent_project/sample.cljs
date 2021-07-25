dd84d8e70745565eeb1f0797c9a76e221d3192c9
ichisemasashi <ichise.masashi@gmail.com>
2021-07-25 16:15:07 +0900

    Reagent project timer-component

--- sample/src/my_reagent_project/sample.cljs
+++ sample/src/my_reagent_project/sample.cljs
@@ -8,17 +8,14 @@
 
 (defn multiply [a b] (* a b))
 
-(def click-count (r/atom 0))
-
-(defn counting-component []
-  [:div
-   "The atom " [:code "click-count"] " has value: "
-   @click-count ". "
-   [:input {:type "button" :value "Click me!"
-            :on-click #(swap! click-count inc)}]])
+(defn timer-component []
+  (let [seconds-elapsed (r/atom 0)]
+    (fn []
+      (js/setTimeout #(swap! seconds-elapsed inc) 1000)
+      [:div "Seconds Elaspsed: " @seconds-elapsed])))
 
 (defn mount []
-  (rdom/render [counting-component] (gdom/getElement "app")))
+  (rdom/render [timer-component] (gdom/getElement "app")))
 
 (mount)
 
