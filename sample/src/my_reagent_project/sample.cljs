fb928c2d254d13463155a6487b5ee2eae69af2e2
ichisemasashi <ichise.masashi@gmail.com>
2021-08-31 21:46:02 +0900

    Reagent Project bmi-component

--- sample/src/my_reagent_project/sample.cljs
+++ sample/src/my_reagent_project/sample.cljs
@@ -8,15 +8,48 @@
 
 (defn multiply [a b] (* a b))
 
-(defn simple-component []
-  [:div
-   [:p "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} "and red "] "text."]])
+(defn calc-bmi [{:keys [height weight bmi] :as data}]
+  (let [h (/ height 100)]
+    (if (nil? bmi)
+      (assoc data :bmi (/ weight (* h h)))
+      (assoc data :weight (* bmi h h)))))
+
+(def bmi-data
+  (r/atom (calc-bmi {:height 180 :weight 80})))
+
+(defn slider [param value min max invalidates]
+  [:input {:type "range" :value value :min min :max max
+           :style {:width "100%"}
+           :on-change (fn [e]
+                        (let [new-value (js/parseInt (.. e -target -value))]
+                          (swap! bmi-data
+                                 (fn [data]
+                                   (-> data
+                                       (assoc param new-value)
+                                       (dissoc invalidates)
+                                       calc-bmi)))))}])
+
+(defn bmi-component []
+  (let [{:keys [weight height bmi]} @bmi-data
+        [color diagnose] (cond (< bmi 18.5) ["orange" "underweight"]
+                               (< bmi 25) ["inherit" "normal"]
+                               (< bmi 30) ["orange" "overweight"]
+                               :else ["red" "obese"])]
+    [:div
+     [:h3 "BMI calculator"]
+     [:div
+      "Height: " (int height) "cm"
+      [slider :height height 100 220 :bmi]]
+     [:div
+      "Weight: " (int weight) "kg"
+      [slider :weight weight 30 150 :bmi]]
+     [:div
+      "BMI: " (int bmi) " "
+      [:span {:style {color color}} diagnose]
+      [slider :bmi bmi 10 50 :weight]]]))
 
 (defn mount []
-  (rdom/render [simple-component] (gdom/getElement "app")))
+  (rdom/render [bmi-component] (gdom/getElement "app")))
 
 (mount)
 
