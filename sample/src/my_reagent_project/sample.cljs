6f5aef35ab3ac83d3d784c597ec2a54a576d55c3
ichisemasashi <ichise.masashi@gmail.com>
2021-09-02 21:11:30 +0900

    Reagent project simple-example(clock)

--- sample/src/my_reagent_project/sample.cljs
+++ sample/src/my_reagent_project/sample.cljs
@@ -2,54 +2,45 @@
   (:require
    [goog.dom :as gdom]
    [reagent.core :as r :refer [atom]]
-   [reagent.dom :as rdom]))
+   [reagent.dom :as rdom]
+   [clojure.string :as str]))
 
 (println "This text is printed from src/my_reagent_project/sample.cljs. Go ahead and edit it and see reloading in action.")
 
 (defn multiply [a b] (* a b))
 
-(defn calc-bmi [{:keys [height weight bmi] :as data}]
-  (let [h (/ height 100)]
-    (if (nil? bmi)
-      (assoc data :bmi (/ weight (* h h)))
-      (assoc data :weight (* bmi h h)))))
+(defonce timer (r/atom (js/Date.)))
 
-(def bmi-data
-  (r/atom (calc-bmi {:height 180 :weight 80})))
+(defonce time-color (r/atom "#f34"))
 
-(defn slider [param value min max invalidates]
-  [:input {:type "range" :value value :min min :max max
-           :style {:width "100%"}
-           :on-change (fn [e]
-                        (let [new-value (js/parseInt (.. e -target -value))]
-                          (swap! bmi-data
-                                 (fn [data]
-                                   (-> data
-                                       (assoc param new-value)
-                                       (dissoc invalidates)
-                                       calc-bmi)))))}])
+(defonce time-updater (js/setInterval
+                       #(reset! timer (js/Date.)) 1000))
 
-(defn bmi-component []
-  (let [{:keys [weight height bmi]} @bmi-data
-        [color diagnose] (cond (< bmi 18.5) ["orange" "underweight"]
-                               (< bmi 25) ["inherit" "normal"]
-                               (< bmi 30) ["orange" "overweight"]
-                               :else ["red" "obese"])]
-    [:div
-     [:h3 "BMI calculator"]
-     [:div
-      "Height: " (int height) "cm"
-      [slider :height height 100 220 :bmi]]
-     [:div
-      "Weight: " (int weight) "kg"
-      [slider :weight weight 30 150 :bmi]]
-     [:div
-      "BMI: " (int bmi) " "
-      [:span {:style {color color}} diagnose]
-      [slider :bmi bmi 10 50 :weight]]]))
+(defn greeting [message]
+  [:h1 message])
+
+(defn clock []
+  (let [time-str (-> @timer .toTimeString (str/split " ") first)]
+    [:div.example-clock
+     {:style {:color @time-color}}
+     time-str]))
+
+(defn color-input []
+  [:div.color-input
+   "Time color: "
+   [:input {:type "text"
+            :value @time-color
+            :on-change #(reset! time-color (-> % .-target .-value))}]])
+
+(defn simple-example []
+  [:div
+   [greeting "Hello world, it is now"]
+   [clock]
+   [color-input]])
+
 
 (defn mount []
-  (rdom/render [bmi-component] (gdom/getElement "app")))
+  (rdom/render [simple-example] (gdom/getElement "app")))
 
 (mount)
 
