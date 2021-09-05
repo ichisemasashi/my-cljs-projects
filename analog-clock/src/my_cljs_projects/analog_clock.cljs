8f8e715b8fa11e93fa83be8850404ec0b1118c35
ichisemasashi <ichise.masashi@gmail.com>
2021-09-05 23:58:37 +0900

    Analog Clock Project 08

--- analog-clock/src/my_cljs_projects/analog_clock.cljs
+++ analog-clock/src/my_cljs_projects/analog_clock.cljs
@@ -20,6 +20,12 @@
 (.setAttribute @cvs "width" mini-side)
 (.setAttribute @cvs "height" mini-side)
 
+;; キャンバスの描画コンテキストを取得
+(def context (reagent/atom (.getContext @cvs "2d")))
+
+;; 描画の原点をキャンバスの中心にセット
+(.translate @context hankei hankei)
+
 ;; キャンバスのstyle要素設定関数
 (defn- set-cvs-style [cvs]
   (let [style (.-style @cvs)
@@ -41,13 +47,36 @@
       (set! (.-padding style) "0 0 0 0")
       (set! (.-margin style) "0 0 0 0"))))
 
-(set-cvs-style cvs)
+;(set-cvs-style cvs)
 
-;; キャンバスの描画コンテキストを取得
-(def context (reagent/atom (.getContext @cvs "2d")))
+(defn div-with-canvas []
+  (let [m (js/parseInt mini-side)
+        h (js/parseInt (.-clientHeight @view-elm))
+        w (js/parseInt (.-clientWidth @view-elm))]
+     [:canvas {:width m, :height m
+               :style {:width (str m "px")
+                       :height (str m "px")
+                       :top (str (/ (- h m) 2) "px")
+                       :left (str (/ (- w m) 2) "px")
+                       :position "absolute"
+                       :boxSizing "border-box"
+                       :border "0"
+                       :padding "0 0 0 0"
+                       :margin "0 0 0 0"}}]))
+(defn centerCircle [ctx r color]
+  (let [h (js/parseInt r)]
+    (set! (.-lineWidth @ctx) 0)
+    (.beginPath @ctx)
+    (aset @ctx "fillStyle" color)
+    (.arc @ctx 0 0 (- h 4) 0 (* js/Math.PI 2))
+    (.fill @ctx)
+    (.stroke @ctx)))
 
-;; 描画の原点をキャンバスの中心にセット
-(.translate @context hankei hankei) ;; ★★ 何となく効いてなさそう。
-;; キャンバスをHTMLに追加
-(.appendChild @view-elm @cvs)
 
+(defn mount []
+  (rdom/render [div-with-canvas] (gdom/getElement "app")))
+
+(js/setInterval mount 1000)
+
+(defn ^:after-load on-reload []
+  (mount))
