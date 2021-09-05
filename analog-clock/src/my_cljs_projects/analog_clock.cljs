05aa4b5ec63ce3af830f2a2116a1ee3b427c4c7a
ichisemasashi <ichise.masashi@gmail.com>
2021-09-05 17:24:38 +0900

    Analog Clock Project 05

--- analog-clock/src/my_cljs_projects/analog_clock.cljs
+++ analog-clock/src/my_cljs_projects/analog_clock.cljs
@@ -6,32 +6,48 @@
 
 (println "This text is printed from src/my_cljs_projects/analog_clock.cljs. Go ahead and edit it and see reloading in action.")
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+;; div要素を取得
+(def view-elm (reagent/atom (gdom/getElement "app")))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+;; div要素の縦横の短い方を取得
+(def mini-side (min (.-clientWidth @view-elm) (.-clientHeight @view-elm)))
+;; 時計の半径を取得
+(def hankei (/ mini-side 2))
+;; キャンバスの作成
+(def cvs (reagent/atom (gdom/createElement "canvas")))
 
-(defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/my_cljs_projects/analog_clock.cljs and watch it change!"]])
+;; キャンバスの描画サイズセット
+(.setAttribute @cvs "width" mini-side)
+(.setAttribute @cvs "height" mini-side)
 
-(defn mount [el]
-  (rdom/render [hello-world] el))
+;; キャンバスのstyle要素設定関数
+(defn- set-cvs-style [cvs]
+  (let [style (.-style @cvs)
+        m (js/parseInt mini-side)
+        h (js/parseInt view-elm.clientHeight)
+        w (js/parseInt view-elm.clientWidth)]
+    (do ;; キャンバスの表示サイズセット
+      (set! (.-width style) (str m "px"))
+      (set! (.-height style) (str m "px"))
+      ;; キャンバスをdiv要素の中央にセット
+      (set! (.-top style)
+            (str (/ (- h m) 2) "px"))
+      (set! (.-left style)
+            (str (/ (- w m) 2) "px"))
+      ;; キャンバスにスタイルをセット
+      (set! (.-position style) "absolute")
+      (set! (.-boxSizing style) "border-box")
+      (set! (.-border style) "0")
+      (set! (.-padding style) "0 0 0 0")
+      (set! (.-margin style) "0 0 0 0"))))
 
-(defn mount-app-element []
-  (when-let [el (get-app-element)]
-    (mount el)))
+(set-cvs-style cvs)
 
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
-(mount-app-element)
+;; キャンバスの描画コンテキストを取得
+(def context (reagent/atom (.getContext @cvs "2d")))
 
-;; specify reload hook with ^;after-load metadata
-(defn ^:after-load on-reload []
-  (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+;; 描画の原点をキャンバスの中心にセット
+(.translate @context hankei hankei) ;; ★★ 何となく効いてなさそう。
+;; キャンバスをHTMLに追加
+(.appendChild @view-elm @cvs)
+
